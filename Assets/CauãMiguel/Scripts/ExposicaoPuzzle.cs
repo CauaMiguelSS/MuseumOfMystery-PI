cb4f38eb938f886795382e9c94d910c382f23d98
CauaMiguelSS <caua.santos9@alunos.sc.senac.br>
2026-08-24 10:45:15 -0300

    build ruim

--- Assets/CauãMiguel/Scripts/ExposicaoPuzzle.cs
+++ Assets/CauãMiguel/Scripts/ExposicaoPuzzle.cs
@@ -2,17 +2,15 @@ using UnityEngine;
 
 public class ExposicaoPuzzle : MonoBehaviour
 {
-    [Header("Quantidade de esculturas")]
     [SerializeField] private int quantidadeTotal = 4;
 
     private int quantidadeCorreta = 0;
 
-    [Header("Objeto para ativar quando terminar")]
     [SerializeField] private GameObject objetoAoCompletar;
 
     public void EsculturaColocada()
     {
-        quantidadeCorreta++;    
+        quantidadeCorreta++;
 
         if (quantidadeCorreta >= quantidadeTotal)
         {
@@ -24,7 +22,7 @@ public class ExposicaoPuzzle : MonoBehaviour
     {
         if (objetoAoCompletar != null)
         {
-            objetoAoCompletar.SetActive(false);
+            Destroy(objetoAoCompletar);
         }
     }
 }
