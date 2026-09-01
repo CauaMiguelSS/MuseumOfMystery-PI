99a08a909f277c33b8a750576c559e51dc6439d2
CauaMiguelSS <caua.santos9@alunos.sc.senac.br>
2026-09-01 09:39:18 -0300

    outiline e vidro

--- Assets/CauãMiguel/Scripts/ExposicaoPuzzle.cs
+++ Assets/CauãMiguel/Scripts/ExposicaoPuzzle.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class ExposicaoPuzzle : MonoBehaviour
@@ -7,6 +8,7 @@ public class ExposicaoPuzzle : MonoBehaviour
     private int quantidadeCorreta = 0;
 
     [SerializeField] private GameObject objetoAoCompletar;
+
 
     public void EsculturaColocada()
     {
